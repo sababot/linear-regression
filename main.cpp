e7d9a02b39ff6bf7d2386487cdfb778b69562314
sababot <twinsgardos@gmail.com>
2021-11-12 22:07:47 +0100

    weights

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "math.h"
+#include <iostream>
+
+using namespace std;
+
+/*------------------- Variables -------------------*/
+const char* FILENAME = "test.csv";
+int MAX_ITERATION = 1000;
+float LEARNING_RATE = 0.1;
+
+/*-------------------- Weights --------------------*/
+class Wieghts
+{
+private:
+	int MAX_WEIGHTS;
+
+public:
+	float* values;
+	int number_weights;
+}
+
+int main()
+{
+	// Code
+}
