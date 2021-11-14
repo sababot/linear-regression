46f75dc9ba1dfc518ccdb29758b696d072296666
sababot <twinsgardos@gmail.com>
2021-11-14 22:33:59 +0100

    weights added

--- main.cpp
+++ main.cpp
@@ -17,6 +17,27 @@ private:
 public:
 	float* values;
 	int number_weights;
+
+	Weights(){};
+
+	Weights(int number_predictor)
+	{
+		number_weights = number_predictor;
+		values = (float *) std::calloc(number_weights, sizeof(float));
+	};
+
+	void update(float **X, float *y, float *y_pred, float learning_rate, int length)
+	{
+		float multiplier = learning_rate/length;
+
+		// Update each weight
+		for (int i = 0; i < number_weights; i++)
+		{
+			float sum = (sum_residual(X, y, y_pred, i, length));
+			printf("Sum = %f\n", sum);
+			values[i] = values[i] - multiplier * sum;
+		}
+	}
 }
 
 int main()
